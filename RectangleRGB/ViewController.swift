0ab3890da994689cccbbae0ab72ee28c64bdfde0
Denis Denisov <denis.denisov8701@gmail.com>
2024-02-02 16:29:43 +0100

    rectangle color changing logic

--- RectangleRGB/ViewController.swift
+++ RectangleRGB/ViewController.swift
@@ -19,25 +19,48 @@ final class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    private var currentRedLabelValue = 0.33
+    private var currentGreenLabelValue = 0.65
+    private var currentBlueLabelValue = 0.22
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         rectangleView.layer.cornerRadius = 10
-        
+        rectangleView.backgroundColor = UIColor(
+            red: currentRedLabelValue,
+            green: currentGreenLabelValue,
+            blue: currentBlueLabelValue,
+            alpha: 1
+        )
     }
     
     @IBAction func sliderAction(_ sender: UISlider) {
         switch sender {
         case redSlider:
+            currentRedLabelValue = Double(sender.value)
             changeValue(in: redValueLabel, sender)
+            changeReactangleColor()
         case greenSlider:
-            changeValue(in: greenValueLabel, sender)
+            currentGreenLabelValue = Double(sender.value)
+            changeValue(in: redValueLabel, sender)
+            changeReactangleColor()
         case blueSlider:
-            changeValue(in: blueValueLabel, sender)
+            currentBlueLabelValue = Double(sender.value)
+            changeValue(in: redValueLabel, sender)
+            changeReactangleColor()
         default:
             return
         }
     }
     
+    private func changeReactangleColor() {
+        rectangleView.backgroundColor = UIColor(
+            red: CGFloat(currentRedLabelValue),
+            green: CGFloat(currentGreenLabelValue),
+            blue: CGFloat(currentBlueLabelValue),
+            alpha: 1
+        )
+    }
     
     private func changeValue(in value: UILabel, _ slider: UISlider) {
         value.text = slider.value.formatted(
