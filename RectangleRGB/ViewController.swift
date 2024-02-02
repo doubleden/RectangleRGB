250292495783a889dcbf23e876b17fb804e50713
Denis Denisov <denis.denisov8701@gmail.com>
2024-02-02 15:58:10 +0100

    label value changing logic

--- RectangleRGB/ViewController.swift
+++ RectangleRGB/ViewController.swift
@@ -8,14 +8,42 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    
     @IBOutlet var rectangleView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         rectangleView.layer.cornerRadius = 10
+        
     }
-
-
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        switch sender {
+        case redSlider:
+            changeValue(in: redValueLabel, sender)
+        case greenSlider:
+            changeValue(in: greenValueLabel, sender)
+        case blueSlider:
+            changeValue(in: blueValueLabel, sender)
+        default:
+            return
+        }
+    }
+    
+    
+    private func changeValue(in value: UILabel, _ slider: UISlider) {
+        value.text = slider.value.formatted(
+            .number.precision(
+                .fractionLength(2)
+            )
+        )
+    }
 }
-
