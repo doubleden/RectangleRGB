36b60a1e587d72d6b470a9c97c911699e15869f3
Denis Denisov <denis.denisov8701@gmail.com>
2024-02-20 23:47:33 +0100

    i dont know what and how i did it fck...

--- RectangleRGB/ViewControllers/SettingViewController.swift
+++ RectangleRGB/ViewControllers/SettingViewController.swift
@@ -30,32 +30,38 @@ final class SettingViewController: UIViewController {
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupKeyBoard()
+        redValueTF.delegate = self
+        greenValueTF.delegate = self
+        blueValueTF.delegate = self
+        
         rectangleView.layer.cornerRadius = 10
-        rectangleView.backgroundColor = color
         updateSlidersValue()
+        updateRectAngleColor()
+        updateTextFieldValue()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
     // MARK: - IB Actions
     @IBAction func sliderAction(_ sender: UISlider) {
-        rectangleView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
-            alpha: 1
-        )
+        updateRectAngleColor()
         updateValueLabel(sender)
     }
 
     @IBAction func doneButtonAction() {
+        view.endEditing(true)
         delegate?.setBackgroundColor(rectangleView.backgroundColor ?? .white)
         dismiss(animated: true)
     }
 }
 
 
-// MARK: - Enum: Slider Type
+// MARK: - Enum: Color
 extension SettingViewController {
-    enum SliderType: Int {
+    enum Color: Int {
         case red = 1
         case green = 2
         case blue = 3
@@ -77,7 +83,7 @@ private extension SettingViewController {
     
     // MARK: - Updatable Methods
     func updateValueLabel(_ slider: UISlider) {
-        if let sliderType = SliderType(rawValue: slider.tag) {
+        if let sliderType = Color(rawValue: slider.tag) {
             switch sliderType {
             case .red:
                 redValueLabel.text = String(format: "%.2f", slider.value)
@@ -89,6 +95,12 @@ private extension SettingViewController {
         }
     }
     
+    func updateTextFieldValue() {
+        redValueTF.text = redSlider.value.formatted()
+        greenValueTF.text = greenSlider.value.formatted()
+        blueValueTF.text = blueSlider.value.formatted()
+    }
+    
     func updateSlidersValue() {
         let colorComponents = getRGBComponents(color)
         
@@ -98,6 +110,51 @@ private extension SettingViewController {
         
         [redSlider, greenSlider, blueSlider].forEach {
             updateValueLabel($0)
+        }
+    }
+    
+    func updateRectAngleColor() {
+        rectangleView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    func setupKeyBoard() {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "done", style: .done, target: self, action: #selector(doneButtonAction))
+        let spaceInToolBar = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        toolbar.setItems([spaceInToolBar, doneButton], animated: false)
+        [redValueTF, greenValueTF, blueValueTF].forEach { $0.inputAccessoryView = toolbar }
+    }
+}
+
+// MARK: - UI TextFieldDelegate
+extension SettingViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        
+        let numberTF = Float(textField.text?.replacingOccurrences(of: ",", with: ".") ?? "")
+        switch textField {
+        case redValueTF:
+            redSlider.value = numberTF ?? 0
+            updateValueLabel(redSlider)
+            updateRectAngleColor()
+        case greenValueTF:
+            greenSlider.value = numberTF ?? 0
+            updateValueLabel(greenSlider)
+            updateRectAngleColor()
+        case blueValueTF:
+            blueSlider.value = numberTF ?? 0
+            updateValueLabel(blueSlider)
+            updateRectAngleColor()
+        default:
+            return
         }
     }
 }
